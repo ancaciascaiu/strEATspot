5f95a9402c8a77322c0bf4d3d022775da3246e4e
kellymp <michellepk89@gmail.com>
2016-09-26 20:45:02 -0500

    Add information to vendor show page. Need styling

--- config/routes.rb
+++ config/routes.rb
@@ -61,4 +61,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :vendors
+    resources :category
 end
