b73da0eda01bb7fda7ef8e3b2470afe0becd9fe5
kellymp <michellepk89@gmail.com>
2016-09-27 14:09:07 -0500

    Add AWS key info to environment. Add attachment association to vendor model

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,6 +3,8 @@ class Vendor < ActiveRecord::Base
   has_many :items
   has_many :vendorscategories
   has_many :foodcategories, through: :vendorscategories
+
+  has_attached_file :profile_picture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
