55140126d622cb10544061151096124690d5a7d4
kellymp <michellepk89@gmail.com>
2016-09-27 16:27:08 -0500

    Add devise permissions to add new avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
     	devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    	 devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:avatar) }
     end
 end
