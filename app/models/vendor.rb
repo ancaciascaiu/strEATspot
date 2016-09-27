97b526abb6c60293523ae1f068933017d8c4ae23
kellymp <michellepk89@gmail.com>
2016-09-27 16:02:44 -0500

    Add migration for avitar. Add attached ability to attach a file in vendor model.

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -4,10 +4,12 @@ class Vendor < ActiveRecord::Base
   has_many :vendorscategories
   has_many :foodcategories, through: :vendorscategories
 
-  has_attached_file :profile_picture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  #hello i am for paperclip
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :large => "400x400#" }, :default_url => "./missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927191442) do
+ActiveRecord::Schema.define(version: 20160927204556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,10 @@ ActiveRecord::Schema.define(version: 20160927191442) do
     t.string   "profile_picture_content_type"
     t.integer  "profile_picture_file_size"
     t.datetime "profile_picture_updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "vendors", ["email"], name: "index_vendors_on_email", unique: true, using: :btree
