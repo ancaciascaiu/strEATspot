bb97aae2accea8bbb0aa3c8b8858e8fbbc3ce96f
Alex Renner & Derick Manliclic <pair+AlexRenner.dmanliclic715@devbootcamp.com>
2016-09-26 20:32:05 -0500

    Created test map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,6 +3,19 @@ var rad = function(x) {
   return x * Math.PI / 180;
 };
 
+// Store all locations
+var markerArray = [
+  {lat: 41.878674, lng: -87.640333},
+  {lat: 41.878274, lng: -87.640330},
+  {lat: 41.878644, lng: -87.640303},
+  {lat: 41.873674, lng: -87.640033},
+  {lat: 41.878670, lng: -87.620333},
+  {lat: 41.858674, lng: -87.640000},
+  {lat: 41.878994, lng: -87.649533},
+  {lat: 41.778674, lng: -87.540333},
+  {lat: 41.878600, lng: -87.644003}
+];
+
 // Gets distance between two coordinates
 var getDistance = function(p1, p2) {
   var R = 6378137; // Earth’s mean radius in meter
@@ -31,10 +44,9 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     'Error: The Geolocation service failed.' :
     'Error: Your browser doesn\'t support geolocation.');
 };
-
+var map;
 // Initialize map
-function initMap() {
-  var image = './dooftruck.png';
+function initMap(markers) {
 
 // Creates a new map instance
   map = new google.maps.Map(document.getElementById('map'), {
@@ -42,9 +54,17 @@ function initMap() {
     zoom: 13 // 1-21 with 21 being fully zoomed in
   });
 
-// Request to view user location
-  var infoWindow = new google.maps.InfoWindow({map: map});
-  var userCoords = {};
+  // Creates the circle
+  var circle = new google.maps.Circle({
+    center: map.center,
+    map: map,
+    radius: 1500, // ~1 MILE IN METERS.
+    fillColor: '#FF6600',
+    fillOpacity: 0.3,
+    strokeColor: "#FFF",
+    strokeWeight: 2
+  });
+
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -52,44 +72,40 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      userCoords = pos;
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
+      var userCoords = new google.maps.LatLng(pos.lat, pos.lng);
+      console.log(pos);
+      circle.center = userCoords;
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
+
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+  var customerPosition = new google.maps.LatLng(circle.center.lat(), circle.center.lng());
+  console.log(circle.center.lat());
+  createMarkers(markerArray, customerPosition, circle);
+};
 
-// Creates the circle
-  var circle = new google.maps.Circle({
-    center: userCoords,
-    map: map,
-    radius: 1000,          // IN METERS.
-    fillColor: '#FF6600',
-    fillOpacity: 0.3,
-    strokeColor: "#FFF",
-    strokeWeight: 0.5         // DON'T SHOW CIRCLE BORDER.
-  });
+var createMarkers = function(markers, userPosition, circle){
+  // var image = './dooftruck.png';
+    console.log(userPosition);
+    console.log(circle.radius);
+
 // Iterates through all stored map markers
-  var customerPosition = new google.maps.LatLng(map.center.lat(), map.center.lng());
-  console.log(customerPosition);
+  for(var i = 0; i < markers.length; i++){
+    var newMarkerCoordinates = new google.maps.LatLng(markers[i].lat, markers[i].lng);
 
-  for(var i = 0; i < markerArray.length; i++){
-    var newMarkerCoordinates = new google.maps.LatLng(markerArray[i].lat, markerArray[i].lng);
-
-    if(radiusCheck(customerPosition, newMarkerCoordinates, circle.radius) == true){
+    if(radiusCheck(userPosition, newMarkerCoordinates, circle.radius) == true){
 
       new google.maps.Marker({
-        position: markerArray[i],
+        position: markers[i],
         map: map,
-        icon: image,
+        // icon: image,
         animation: google.maps.Animation.DROP
       });
 
     } else { };
-  };
+  }
 };
