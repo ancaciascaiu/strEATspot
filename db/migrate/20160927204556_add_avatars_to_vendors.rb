bbe10a2e10dabdd9bca4172cc8f67ccf4c6c67ab
kellymp <michellepk89@gmail.com>
2016-09-28 10:26:39 -0500

    Comment out for heroku test

--- db/migrate/20160927204556_add_avatars_to_vendors.rb
+++ db/migrate/20160927204556_add_avatars_to_vendors.rb
@@ -1,12 +1,12 @@
 class AddAvatarsToVendors < ActiveRecord::Migration
     def self.up
     change_table :vendors do |t|
-      t.attachment :avatar
+      # t.attachment :avatar
     end
   end
 
   def self.down
-    drop_attached_file :vendors, :avatar
+    # drop_attached_file :vendors, :avatar
   end
 end
 
